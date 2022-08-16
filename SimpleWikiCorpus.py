bfaee0504a6c719a399c034fba3fd8abe3c8bf73
John Stegink <74827523+johnstegink@users.noreply.github.com>
2022-08-16 19:31:19 +0200

    Added the ability to generate a SimpleWikiCorpus

--- SimpleWikiCorpus.py
+++ SimpleWikiCorpus.py
@@ -48,7 +48,7 @@ def save_articles(wikidata, lemmata, count, id_prefix, output):
     written = []
 
     for i in range(0, len(lemmata)):
-        if len( written) < count:
+        if len( written)  < count:
             id = f"{id_prefix}_{i + 1}"  # Dummy id
 
             xml = wikidata.read_wikipedia_article(id, lemmata[i])
@@ -61,28 +61,48 @@ def save_articles(wikidata, lemmata, count, id_prefix, output):
     return written
 
 
+def remove_all_files( dir):
+    import glob
+    files = glob.glob(f"{dir}/*")
+    for f in files:
+        os.remove(f)
+
 # Main part of the script
 if __name__ == '__main__':
     (simple, english) = read_arguments()
 
     functions.create_directory_if_not_exists(simple)
     functions.create_directory_if_not_exists(english)
+    remove_all_files( simple)
+    remove_all_files( english)
 
     simplewiki = Wikidata(wikidata_endpoint=wikidata_enpoint, subjects=[], dump_dir=wikipedia_dumpdir,
                         language="simple", debug=False)
+
     enwiki = Wikidata(wikidata_endpoint=wikidata_enpoint, subjects=[], dump_dir=wikipedia_dumpdir,
                         language="en", debug=False)
 
     # 46 Random articles starting with a
-    a_articles = simplewiki.get_random_articles( exceptions=[], count=3 * number_of_random_articles, only_starting_with_a=True)
+    a_articles = ['Alfred Lif', 'American Physical Society', 'Arrondissement of Die', 'Askar Mamin', 'Art exhibition',
+     'Abdelhamid Ben Badis', 'At Home Among Strangers', 'Arrondissements of the Lot-et-Garonne department',
+     'Afrikan tähti', 'Adelomyrmex boltoni', 'Anabolic steroid', 'Alquézar', 'Akebi', 'Ampoigné', 'Albert Coates',
+     'Aichkirchen, Germany', 'Ayaka Kimura', 'Afro-Haitian', 'Alexander Sergeyevich Yakovlev', 'Agatha Barbara',
+     'Aimyon', "Albiano d'Ivrea", 'Arapaho, Oklahoma', 'Alismatales', 'Arlene McQuade', 'Albert Lebrun',
+     'Adhemir de Barros', 'Auxerre', 'Adams County, Iowa', 'Aconcagua', 'Alan Bates', 'Anna Mouglalis',
+     'Andrea Feldman', 'A Man Before His Time', 'Antivenom', 'Artemi Panarin', 'Adrian Fenty', 'Alexander Hickman',
+     'Ace Frehley', 'Abd al-Aziz Bin Baz', 'Assistant secretary', 'Arnhem', 'Adirondack Mountains', 'Arles',
+     'Alexis Jordan', 'Angelo Badalamenti']
+    # a_articles = simplewiki.get_random_articles( exceptions=[], count=3 * number_of_same_articles, only_starting_with_a=True)
+    # a_articles = save_articles(enwiki, a_articles, number_of_same_articles * 2, "a", english)
+    # a_articles = save_articles(simplewiki, a_articles, number_of_same_articles, "a", simple)
 
     # 1000 different articles
     random_simple = simplewiki.get_random_articles( exceptions=a_articles, count=(3 * number_of_random_articles), only_starting_with_a=False)
-    a_articles = save_articles(simplewiki, a_articles, number_of_same_articles, "a", simple)
-    save_articles(simplewiki, random_simple, number_of_random_articles, "r", simple)
+    save_articles(simplewiki, a_articles, number_of_same_articles, "a", simple)
+    simple_articles = save_articles(simplewiki, random_simple, number_of_random_articles, "r", simple)
 
-    # 1000 english articles
-    random_en = enwiki.get_random_articles( exceptions=a_articles, count=(3 * number_of_random_articles), only_starting_with_a=False)
-    save_articles(enwiki, a_articles, number_of_random_articles, "a", english)
-    save_articles(enwiki, a_articles, number_of_random_articles, "r", english)
+    # 1000 english articles, exclude the lemmata used for the a_articles an the simple articles
+    random_en = enwiki.get_random_articles( exceptions=(a_articles + simple_articles), count=(3 * number_of_random_articles), only_starting_with_a=False)
+    save_articles(enwiki, a_articles, number_of_same_articles, "a", english)
+    save_articles(enwiki, random_en, number_of_random_articles, "r", english)
 
